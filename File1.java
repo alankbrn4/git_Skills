8dd6faae5b2dbf8a2eeb5a5ecc095b14a0ad45b3
ALAN47 <alankbrn4@gmail.com>
2023-09-12 18:17:04 -0600

    Version 1.1 - File1.java runCar was created for define the wheels direction

--- File1.java
+++ File1.java
@@ -1,6 +1,12 @@
 public class File1{
     public static void main(String [] args){
         System.out.println("Hola Mundo");
+        runCar(args);
+    }
+
+    public static void runCar(String [] args){
+        System.out.println("Direccion 1 asignada");
+        System.out.println("Dirección 2 asignada");
     }
 
 }
--- File2.java
+++ File2.java
@@ -1,3 +1,5 @@
 public class File2 {
-    
+    public static void main(String [] args){
+        System.out.println("Este es un mensaje del código 2");
+    }
 }
