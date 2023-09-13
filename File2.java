0b4f8b88d419dc881355076c025eeb25315e8e80
ALAN47 <alankbrn4@gmail.com>
2023-09-12 18:24:56 -0600

    Version 1.2 - File2.java checkStatus function was created for check sensors status

--- File2.java
+++ File2.java
@@ -1,5 +1,11 @@
 public class File2 {
     public static void main(String [] args){
         System.out.println("Este es un mensaje del código 2");
+        checkStatus(args);
+    }
+
+    public static void checkStatus(String [] args){
+        System.out.println("El estado del sensor 1 es positivo");
+        System.out.println("El estado del sensor 2 es positivo");
     }
 }
